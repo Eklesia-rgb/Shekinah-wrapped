add674e39df6e07ad344ae14ae8e07a2692b7ab3
Eklesia-rgb <hopeshekie@gmail.com>
2025-12-05 09:56:20 +0300

    Added the footer

--- my-landing-page/script.js
+++ my-landing-page/script.js
@@ -5,13 +5,12 @@ tracks.forEach((track) => {
   track.addEventListener("click", () => {
     const icon = track.querySelector(".icon");
 
-    // Reset all icons
     document.querySelectorAll(".icon").forEach((i) => {
       i.textContent = "▶";
       i.classList.remove("active");
     });
 
-    // If already playing, pause
+    
     if (
       audioPlayer.src === track.getAttribute("data-track") &&
       !audioPlayer.paused
@@ -19,11 +18,11 @@ tracks.forEach((track) => {
       audioPlayer.pause();
       icon.textContent = "▶";
     } else {
-      // Load and play new track
+      
       audioPlayer.src = track.getAttribute("data-track");
       audioPlayer.play();
-      icon.textContent = "❚❚"; // pause icon
-      icon.classList.add("active"); // glowing effect
+      icon.textContent = "❚❚"; 
+      icon.classList.add("active"); 
     }
   });
 });
